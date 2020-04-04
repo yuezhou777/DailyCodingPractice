6184057e83bfc8f52fe3d3d8e6ace87a5fe55e14
yuezhou777 <zhou.yue2@husky.neu.edu>
2020-04-03 23:57:25 -0700

    04/03

---
+++ src/Reverse_Polish_Notation/LC150_Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,29 @@
+package Reverse_Polish_Notation;
+
+import java.util.Stack;
+
+//O(N)
+//O(N)
+public class LC150_Evaluate_Reverse_Polish_Notation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String s : tokens) {
+            if(s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            }else if(s.equals("-")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 - num2);
+            }else if(s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            }else if(s.equals("/")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 / num2);
+            }else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop() ;
+    }
+}
