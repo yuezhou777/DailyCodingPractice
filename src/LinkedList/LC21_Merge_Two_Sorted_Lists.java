fea3e0b0ca74f1d75afe52e9aa9ed710996b8a44
yuezhou777 <zhou.yue2@husky.neu.edu>
2020-07-08 00:58:54 -0700

    07/08

--- src/LinkedList/LC21_Merge_Two_Sorted_Lists.java
+++ src/LinkedList/LC21_Merge_Two_Sorted_Lists.java
@@ -1,5 +1,18 @@
 package LinkedList;
-
+/*
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ */
 public class LC21_Merge_Two_Sorted_Lists {
-
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
