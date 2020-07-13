879c1fc6607f11186731d920321fe751ec571db1
yuezhou777 <zhou.yue2@husky.neu.edu>
2020-07-13 00:26:06 -0700

    07/12

---
+++ src/Stack_and_Queue/LC42_Trapping_Rain_Water.java
@@ -0,0 +1,27 @@
+package Stack_and_Queue;
+
+public class LC42_Trapping_Rain_Water {
+    // size = size(left_Max, right_Max) - duplicates
+    // monotone stack -> 2 pointers
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) return 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int res = 0;
+        while (left < right) {
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
+            if (leftMax <= rightMax) {
+                res += leftMax - height[left];
+                left++;
+            } else {
+                res += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return res;
+    }
+}
